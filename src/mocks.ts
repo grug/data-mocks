727eb1e3271cf3771a5230338698961e9db18749
Kieran Allen <kieran.allen@ovoenergy.com>
2018-12-04 20:28:30 +0000

    upgrade packages, change extractScenarioFromLocation add tests (#26)
    * Upgrade packages to their latest version.
    * removed `uglifyjs` replaced with `uglify-js`.
    * added `--mangle -m` & `--compress -c` to uglify. Dist now comes in under 2KB!
    * Updated unit test to account for method in default not being overwritten.
    * Made changes to `extractScenarioFromLocation`. Added unit tests for function.
    * Remove lodash and replace with native js functions.
    
    Unit tests pass & Tested in local application.

--- src/mocks.ts
+++ src/mocks.ts
@@ -1,7 +1,6 @@
 import * as FetchMock from 'fetch-mock';
 import XHRMock, { delay as xhrMockDelay } from 'xhr-mock';
 import { parse } from 'query-string';
-import { find } from 'lodash';
 
 export type HttpMethod = 'GET' | 'POST' | 'PUT' | 'DELETE';
 
@@ -22,9 +21,11 @@ export interface Scenarios {
  * Gets the corresponding value for `scenario` key in the browser's Location object.
  */
 export const extractScenarioFromLocation = (location: Location): string => {
-  return parse(location.search).scenario
-    ? parse(location.search).scenario
-    : 'default';
+  const { scenario = 'default' } = parse(location.search);
+  if (Array.isArray(scenario)) {
+    throw new Error('Only one scenario may be used at a time');
+  }
+  return scenario;
 };
 
 /**
@@ -106,8 +107,7 @@ export const reduceAllMocksForScenario = (
   return defaultMocks
     .filter(
       d =>
-        !find(
-          scenarioMocks,
+        !scenarioMocks.find(
           s => s.url.toString() === d.url.toString() && d.method === s.method
         )
     )
