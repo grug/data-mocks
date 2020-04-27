8c48c019a6039ee89a8f52d8c84e9c09971f3231
Dave Cooper <dave@davecooper.org>
2020-04-27 20:00:58 +0100

    Add tests for mocks using fetch and tidy up the structure of the file

--- src/mocks.test.ts
+++ src/mocks.test.ts
@@ -3,47 +3,167 @@ import axios from 'axios';
 import {
   injectMocks,
   extractScenarioFromLocation,
-  reduceAllMocksForScenario
+  reduceAllMocksForScenario,
 } from './mocks';
 import { HttpMethod, Scenarios, MockConfig } from './types';
 import XHRMock, { proxy } from 'xhr-mock';
 import fetchMock from 'fetch-mock';
 
 describe('data-mocks', () => {
-  describe('HTTP methods', () => {
-    const httpMethods: HttpMethod[] = ['GET', 'POST', 'PUT', 'DELETE'];
+  describe('REST', () => {
+    describe('HTTP methods', () => {
+      const httpMethods: HttpMethod[] = ['GET', 'POST', 'PUT', 'DELETE'];
 
-    httpMethods.forEach(httpMethod => {
+      httpMethods.forEach((httpMethod) => {
+        const scenarios: Scenarios = {
+          default: [
+            {
+              url: /foo/,
+              method: httpMethod,
+              response: {},
+              responseCode: 200,
+            },
+            {
+              url: /bar/,
+              method: httpMethod,
+              response: {},
+              responseCode: 200,
+            },
+          ],
+        };
+
+        test(`Mocks calls for ${httpMethod}`, () => {
+          const fetchSpy = jest.spyOn(
+            fetchMock,
+            httpMethod.toLowerCase() as any
+          );
+          const xhrSpy = jest.spyOn(XHRMock, httpMethod.toLowerCase() as any);
+
+          injectMocks(scenarios, 'default');
+
+          expect(fetchSpy).toHaveBeenCalledTimes(2);
+          expect(fetchSpy.mock.calls[0][0]).toEqual(/foo/);
+          expect(fetchSpy.mock.calls[1][0]).toEqual(/bar/);
+
+          expect(xhrSpy).toHaveBeenCalledTimes(2);
+          expect(xhrSpy.mock.calls[0][0]).toEqual(/foo/);
+          expect(xhrSpy.mock.calls[1][0]).toEqual(/bar/);
+        });
+      });
+    });
+
+    describe('Fetch mocks', () => {
+      const getMatcher = /get-endpoint/;
+      const postMatcher = /post-endpoint/;
+      const putMatcher = /put-endpoint/;
+      const deleteMatcher = /delete-endpoint/;
+
+      const scenarios: Scenarios = {
+        default: [
+          {
+            url: getMatcher,
+            method: 'GET',
+            response: {},
+            responseCode: 200,
+            responseHeaders: { token: 'foo' },
+          },
+          {
+            url: postMatcher,
+            method: 'POST',
+            response: {},
+            responseCode: 200,
+            responseHeaders: {},
+          },
+          {
+            url: putMatcher,
+            method: 'PUT',
+            response: {},
+            responseCode: 200,
+            responseHeaders: undefined,
+          },
+          {
+            url: deleteMatcher,
+            method: 'DELETE',
+            response: {},
+            responseCode: 200,
+          },
+        ],
+      };
+
+      beforeAll(() => {
+        injectMocks(scenarios, 'default');
+      });
+
+      test(`Correct response for mocked XHR endpoints`, async () => {
+        expect(fetchMock.calls().length === 0).toBeTruthy();
+
+        await fetch('/get-endpoint', { method: 'GET' });
+        expect(fetchMock.called(getMatcher)).toBeTruthy();
+
+        await fetch('/post-endpoint', { method: 'POST' });
+        expect(fetchMock.called(postMatcher)).toBeTruthy();
+
+        await fetch('/put-endpoint', { method: 'PUT' });
+        expect(fetchMock.called(putMatcher)).toBeTruthy();
+
+        await fetch('/delete-endpoint', { method: 'DELETE' });
+        expect(fetchMock.called(deleteMatcher)).toBeTruthy();
+      });
+    });
+
+    describe('XHR mocks', () => {
       const scenarios: Scenarios = {
         default: [
           {
             url: /foo/,
-            method: httpMethod,
-            response: {},
-            responseCode: 200
+            method: 'GET',
+            response: { foo: 'GET' },
+            responseCode: 200,
+            responseHeaders: { token: 'foo' },
           },
           {
-            url: /bar/,
-            method: httpMethod,
-            response: {},
-            responseCode: 200
-          }
-        ]
+            url: /foo/,
+            method: 'POST',
+            response: { foo: 'POST' },
+            responseCode: 200,
+            responseHeaders: {},
+          },
+          {
+            url: /foo/,
+            method: 'PUT',
+            response: { foo: 'PUT' },
+            responseCode: 200,
+            responseHeaders: undefined,
+          },
+          {
+            url: /foo/,
+            method: 'DELETE',
+            response: { foo: 'DELETE' },
+            responseCode: 200,
+          },
+        ],
       };
 
-      test(`Mocks calls for ${httpMethod}`, () => {
-        const fetchSpy = jest.spyOn(fetchMock, httpMethod.toLowerCase() as any);
-        const xhrSpy = jest.spyOn(XHRMock, httpMethod.toLowerCase() as any);
-
+      beforeAll(() => {
         injectMocks(scenarios, 'default');
+      });
 
-        expect(fetchSpy).toHaveBeenCalledTimes(2);
-        expect(fetchSpy.mock.calls[0][0]).toEqual(/foo/);
-        expect(fetchSpy.mock.calls[1][0]).toEqual(/bar/);
+      test(`Correct response for mocked XHR endpoints`, async () => {
+        const resGET = await axios.get('/foo');
+        expect(resGET.data).toEqual({ foo: 'GET' });
+        expect(resGET.headers).toEqual({ token: 'foo' });
 
-        expect(xhrSpy).toHaveBeenCalledTimes(2);
-        expect(xhrSpy.mock.calls[0][0]).toEqual(/foo/);
-        expect(xhrSpy.mock.calls[1][0]).toEqual(/bar/);
+        const resPOST = await axios.post('/foo');
+        expect(resPOST.data).toEqual({ foo: 'POST' });
+        expect(resPOST.headers).toEqual({});
+
+        const resPUT = await axios.put('/foo');
+        expect(resPUT.data).toEqual({ foo: 'PUT' });
+        expect(resPUT.headers).toEqual({});
+
+        const resDELETE = await axios.delete('/foo');
+        expect(resDELETE.data).toEqual({ foo: 'DELETE' });
+        expect(resDELETE.headers).toEqual({});
       });
     });
   });
@@ -56,27 +176,28 @@ describe('data-mocks', () => {
           method: 'GET',
           response: {},
           responseCode: 200,
-          responseHeaders: { token: 'foo' }
+          responseHeaders: { token: 'foo' },
         },
         {
           url: /bar/,
           method: 'GET',
           response: {},
           responseCode: 200,
-          responseHeaders: { token: 'bar' }
+          responseHeaders: { token: 'bar' },
         },
-        { url: /bar/, method: 'POST', response: {}, responseCode: 200 }
+        { url: /bar/, method: 'POST', response: {}, responseCode: 200 },
       ],
       someScenario: [
         {
           url: /bar/,
           method: 'GET',
           response: { some: 'otherResponse' },
-          responseCode: 401
+          responseCode: 401,
         },
-        { url: /baz/, method: 'POST', response: {}, responseCode: 200 }
-      ]
+        { url: /baz/, method: 'POST', response: {}, responseCode: 200 },
+      ],
     };
+
     test(`Can extract the scenario from anywhere in the URL`, () => {
       window.history.pushState(
         {},
@@ -96,16 +217,16 @@ describe('data-mocks', () => {
           method: 'GET',
           response: {},
           responseCode: 200,
-          responseHeaders: { token: 'foo' }
+          responseHeaders: { token: 'foo' },
         },
         {
           url: /bar/,
           method: 'GET',
           response: {},
           responseCode: 200,
-          responseHeaders: { token: 'bar' }
+          responseHeaders: { token: 'bar' },
         },
-        { url: /bar/, method: 'POST', response: {}, responseCode: 200 }
+        { url: /bar/, method: 'POST', response: {}, responseCode: 200 },
       ]);
     });
 
@@ -118,30 +239,30 @@ describe('data-mocks', () => {
           method: 'GET',
           response: {},
           responseCode: 200,
-          responseHeaders: { token: 'foo' }
+          responseHeaders: { token: 'foo' },
         },
         {
           url: /bar/,
           method: 'GET',
           response: { some: 'otherResponse' },
-          responseCode: 401
+          responseCode: 401,
         },
         {
           url: /bar/,
           method: 'POST',
           response: {},
-          responseCode: 200
+          responseCode: 200,
         },
-        { url: /baz/, method: 'POST', response: {}, responseCode: 200 }
+        { url: /baz/, method: 'POST', response: {}, responseCode: 200 },
       ]);
     });
 
     test(`Returns default mocks if user specifies scenario with no defined mocks`, () => {
       const scenarios: Scenarios = {
         default: [
-          { url: /foo/, method: 'GET', response: {}, responseCode: 200 }
+          { url: /foo/, method: 'GET', response: {}, responseCode: 200 },
         ],
-        scenario: []
+        scenario: [],
       };
       const result = reduceAllMocksForScenario(scenarios, 'scenario');
       expect(result).toEqual(scenarios.default);
@@ -150,70 +271,14 @@ describe('data-mocks', () => {
     test(`Returns empty array if default and scenario mocks are not defined`, () => {
       const scenarios: Scenarios = {
         default: [],
-        scenario: []
+        scenario: [],
       };
       const result = reduceAllMocksForScenario(scenarios, 'scenario');
       expect(result).toEqual([]);
     });
   });
 
-  describe('XHR mock calls', () => {
-    const scenarios: Scenarios = {
-      default: [
-        {
-          url: /foo/,
-          method: 'GET',
-          response: { foo: 'GET' },
-          responseCode: 200,
-          responseHeaders: { token: 'foo' }
-        },
-        {
-          url: /foo/,
-          method: 'POST',
-          response: { foo: 'POST' },
-          responseCode: 200,
-          responseHeaders: {}
-        },
-        {
-          url: /foo/,
-          method: 'PUT',
-          response: { foo: 'PUT' },
-          responseCode: 200,
-          responseHeaders: undefined
-        },
-        {
-          url: /foo/,
-          method: 'DELETE',
-          response: { foo: 'DELETE' },
-          responseCode: 200
-        }
-      ]
-    };
-
-    beforeAll(() => {
-      injectMocks(scenarios, 'default');
-    });
-
-    test(`Correct response for mocked XHR endpoints`, async () => {
-      const resGET = await axios.get('/foo');
-      expect(resGET.data).toEqual({ foo: 'GET' });
-      expect(resGET.headers).toEqual({ token: 'foo' });
-
-      const resPOST = await axios.post('/foo');
-      expect(resPOST.data).toEqual({ foo: 'POST' });
-      expect(resPOST.headers).toEqual({});
-
-      const resPUT = await axios.put('/foo');
-      expect(resPUT.data).toEqual({ foo: 'PUT' });
-      expect(resPUT.headers).toEqual({});
-
-      const resDELETE = await axios.delete('/foo');
-      expect(resDELETE.data).toEqual({ foo: 'DELETE' });
-      expect(resDELETE.headers).toEqual({});
-    });
-  });
-
-  describe('Extract scenario from location', () => {
+  describe('Utility: extractScenarioFromLocation', () => {
     test(`Correct scenario name is returned`, () => {
       window.history.pushState({}, 'Test', '/?scenario=test');
       expect(extractScenarioFromLocation(window.location)).toBe('test');
@@ -232,16 +297,16 @@ describe('data-mocks', () => {
     });
   });
 
-  describe('Mock config', () => {
+  describe('Config', () => {
     const scenarios: Scenarios = {
       default: [
         {
           url: /foo/,
           method: 'GET',
           response: { foo: 'GET' },
-          responseCode: 200
-        }
-      ]
+          responseCode: 200,
+        },
+      ],
     };
 
     beforeEach(() => {
@@ -252,7 +317,7 @@ describe('data-mocks', () => {
       const xhrSpy = jest.spyOn(XHRMock, 'use');
 
       const mockConfig: MockConfig = {
-        allowXHRPassthrough: true
+        allowXHRPassthrough: true,
       };
 
       injectMocks(scenarios, 'default', mockConfig);
@@ -264,7 +329,7 @@ describe('data-mocks', () => {
       const xhrSpy = jest.spyOn(XHRMock, 'use');
 
       const mockConfig: MockConfig = {
-        allowXHRPassthrough: false
+        allowXHRPassthrough: false,
       };
 
       injectMocks(scenarios, 'default', mockConfig);
@@ -282,7 +347,7 @@ describe('data-mocks', () => {
 
     test('Sets fallbackToNetwork to false if allowFetchPassthrough is set to false in config', () => {
       const mockConfig: MockConfig = {
-        allowFetchPassthrough: false
+        allowFetchPassthrough: false,
       };
       injectMocks(scenarios, 'default', mockConfig);
       expect(fetchMock.config.fallbackToNetwork).toBe(false);
@@ -296,7 +361,7 @@ describe('data-mocks', () => {
 
     test('Sets fallbackToNetwork to true if allowFetchPassthrough is set to true in config', () => {
       const mockConfig: MockConfig = {
-        allowFetchPassthrough: true
+        allowFetchPassthrough: true,
       };
       injectMocks(scenarios, 'default', mockConfig);
       expect(fetchMock.config.fallbackToNetwork).toBe(true);
@@ -304,7 +369,7 @@ describe('data-mocks', () => {
 
     test('Returns mock data if allowXHRPassthrough is set to true and route exists as mock', async () => {
       const mockConfig: MockConfig = {
-        allowXHRPassthrough: true
+        allowXHRPassthrough: true,
       };
       injectMocks(scenarios, 'default', mockConfig);
       const resGET = await axios.get('/foo');
@@ -315,7 +380,7 @@ describe('data-mocks', () => {
       // We only expect an error in this case becaue the route does not exist.
       // We just want to see if we attempted a real network request here.
       const mockConfig: MockConfig = {
-        allowXHRPassthrough: true
+        allowXHRPassthrough: true,
       };
       injectMocks(scenarios, 'default', mockConfig);
       try {
